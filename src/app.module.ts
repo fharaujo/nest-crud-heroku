6a3898a05f463dc1b2b2c8476297d3ed50acacd0
fharaujo <faraujodeveloper@hotmail.com>
2021-09-21 21:36:43 -0300

    connect mongoose and variable connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 
+import { ConfigModule } from '@nestjs/config';
+ConfigModule.forRoot();
+const dbbUse = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${dbbUse}:${dbPassword}@cluster0.ccw2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
