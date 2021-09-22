1b90a52d57dc0543a908a517cc22fc6931a1fb88
fharaujo <faraujodeveloper@hotmail.com>
2021-09-21 22:42:33 -0300

    refactory env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { UsersModule } from './users/users.module';
 
 import { ConfigModule } from '@nestjs/config';
 ConfigModule.forRoot();
-const dbbUse = process.env.DB_USER;
+const dbUse = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://${dbbUse}:${dbPassword}@cluster0.ccw2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+      `mongodb+srv://${dbUse}:${dbPassword}@cluster0.ccw2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     ),
     UsersModule,
   ],
